ffdaab700a465a7e8a43d801d25130a53abfc1f0
davirbeiro-1 <davirbeiro1@hotmail.com>
2021-04-26 22:41:23 -0300

    :construction: Adding a theme music

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pokedex
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -12,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        playPokeTheme()
+
         val initialButton = findViewById<Button>(R.id.initialButton)
         initialButton.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun playPokeTheme() {
+        val ring = MediaPlayer.create(this@MainActivity, R.raw.song);
+        ring.start();
     }
 }
